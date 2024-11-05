945a3ad94b84ce1c233c17c4256c0d07fb307916
unknown <11Rahulsinghrawat@gmail.com>
2024-11-05 12:42:32 +0530

    Added AboutPage image

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,29 +1,38 @@
 import React from 'react'
 import PeopleReviews from '../components/About/PeopleReviews'
+import AboutImage from '../Images/About_Image.png'
 
 const About = () => {
     return (
         <div className=' text-white p-4' id='AboutParentContainer'>
             <p className='text-4xl font-bold text-wrap w-[90%] p-4'>Country-Club was created to serve the needs of users and providers of adult services</p>
-            <div className='mt-4 p-4'>
-                <p className='text-2xl font-semibold'>
-                    What is it for?
-                </p>
-                <p>
-                    Providers can list a detailed and information profile in Country spa where they can propose their time and companionship to others. Users of the site can then make contact directly. We do not act as an intermediary or play any role in any transaction which may eventually take place. We recommend you follow the local laws of the country in which you reside.
-                </p>
-            </div>
-            <div className='mt-4 p-4'>
-                <p className='text-2xl font-semibold'>
-                    Why are we different?
-                </p>
-                <p>
-                    We have spent a lot of time communicating with users and advertisers to produce a website which is as useful as possible. Features such as verified photos, user reviews, questions and SSL security all help this to happen. We want users and providers to find Country spa useful and are working hard to make that happen.
-                </p>
+            <div className=' sm:flex sm:flex-row-reverse'>
+                <div className=''>
+                    <img src={AboutImage} alt='about image' />
+                </div>
+                <div className='w-3/4'>
+                    <div className='mt-4 p-4'>
+                        <p className='text-2xl font-semibold'>
+                            What is it for?
+                        </p>
+                        <p>
+                            Providers can list a detailed and information profile in Country spa where they can propose their time and companionship to others. Users of the site can then make contact directly. We do not act as an intermediary or play any role in any transaction which may eventually take place. We recommend you follow the local laws of the country in which you reside.
+                        </p>
+                    </div>
+                    <div className='mt-4 p-4'>
+                        <p className='text-2xl font-semibold'>
+                            Why are we different?
+                        </p>
+                        <p>
+                            We have spent a lot of time communicating with users and advertisers to produce a website which is as useful as possible. Features such as verified photos, user reviews, questions and SSL security all help this to happen. We want users and providers to find Country spa useful and are working hard to make that happen.
+                        </p>
+                    </div>
+                </div>
             </div>
 
+
             <div className='mt-4 p-4' id='peopleReviews'>
-                <PeopleReviews/>
+                <PeopleReviews />
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logoImage from '../Images/logoImage.jpg';
 import PersonIcon from '@mui/icons-material/Person';
-import SearchIcon from '@mui/icons-material/Search';
 import LanguageIcon from '@mui/icons-material/Language';
 import { Link } from 'react-router-dom';
 
@@ -28,19 +27,6 @@ export const Navbar = () => {
           </button>
         </div>
       </div>
-        <div className='flex items-center justify-center flex-wrap py-4 gap-4 mb-4' id='searchFilter'style={{ background: 'linear-gradient(rgb(31 29 29), rgb(14 14 14))' }}>
-          <select name="location" id="location" className='py-2 rounded-md px-2 text-gray-600'>
-            <option value="delhi">Delhi</option>
-            <option value="noida">Noida</option>
-            <option value="gurugram">Gurugram</option>
-            <option value="new delhi">New Delhi</option>
-          </select>
-          <input type='text' className='border-2 border-black  rounded-lg focus:border-none py-1 text-center' placeholder='Search' />
-          <button className=' bg-yellow-500 px-1 rounded-md font-bold space-x-2 py-1'>
-            Search
-            <SearchIcon className=' hover:cursor-pointer' fontSize='medium' sx={{ color: 'white' }} />
-          </button>
-        </div>
     </>
   );
 };
